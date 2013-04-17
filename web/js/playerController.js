0f062d6621234d7bd70edec62d044f2c4823d78f
alfredoo <alfredo.uy@gmail.com>
2013-04-16 23:52:22 -0300

    lo que tenemos para arrancar!

---
+++ web/js/playerController.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: alfredoo
+ * Date: 16/04/13
+ * Time: 11:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var Ikusi = Ikusi | {};
+var Ikusi.org = Ikusi.org | {};
+var Ikusi.org.VideoController = {
+
+
+}

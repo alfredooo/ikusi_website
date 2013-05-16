64a6e0bb5aba873581c125fcd773d1d0864d5a52
alfredoo <alfredo.uy@gmail.com>
2013-05-15 21:04:33 -0300

    - trabajo en api para reproducir videos youtube. - actualizaciones varias.

---
+++ server.js
@@ -0,0 +1,42 @@
+var fs      = require('fs'),
+    mime    = require('mime'),
+    http    = require('http');
+
+process.on('uncaughtException', function (err) {
+    console.error(err.stack);
+    console.log("Node NOT Exiting...");
+});
+
+//Create server
+var port =  8081;
+var server  = http.createServer(init).listen(port);
+
+function init(req, res) {
+    var path ='web';
+
+    if(req.url == '/'){
+        path = path + '/index.html'
+    }
+    else{
+        path = path + req.url;
+    }
+
+    fs.exists(path,function(exist){
+        if(exist){
+            fs.readFile(path,function(error,content){
+                if (error){
+                    res.writeHead(500);
+                    res.end();
+                    console.log("here");
+                } else {
+                    res.writeHead(200,{'Content-Type':mime.lookup(path)});
+                    res.end(content);
+                }
+            });
+        }
+        else{
+            res.writeHead(404);
+            res.end();
+        }
+    });
+}

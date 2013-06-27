d1d12cc9f4cd11681aa5bf9cfda95ccf76794f79
OpenShift guest <alfredo.uy@gmail.com>
2013-06-27 04:35:33 -0400

    initial repo ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ikusi::Application.config.secret_token = '7818d006fc0e201f54ceea935db2cbee9aa7ebbf654fbfaf2e32fb9479f6856ad49b1a2ff39d12b5f968810dd2a2aa17e1e67803dff6feee8a0942b3710d067f'
